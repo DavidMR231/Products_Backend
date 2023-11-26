c5afc4d036df9bb96b4afbe15f2d45d6ecd2d118
DAVIDMR <morenorojasdavidsantiago@gmail.com>
2023-11-25 22:34:36 -0500

    Mejora del API

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const { Pool } = require("pg");
 const express = require("express");
 const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-require("dotenv").config();
+const PORT = process.env.PORT;
 const API_KEY = process.env.API_KEY;
+
+// Configuración de la piscina de PostgreSQL
+const pool = new Pool({
+  user: "default",
+  host: "ep-quiet-sun-10474282-pooler.us-east-1.postgres.vercel-storage.com",
+  database: "verceldb",
+  password: "q8hEO7VZtzrl",
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
+});
+
+// Middleware para analizar el cuerpo de la solicitud como JSON
+app.use(express.json());
+
+// Middleware para validar la API key
 const apiKeyValidation = (req, res, next) => {
   const userApiKey = req.get("x-api-key");
   if (userApiKey && userApiKey === API_KEY) {
@@ -16,78 +28,92 @@ const apiKeyValidation = (req, res, next) => {
 };
 app.use(apiKeyValidation);
 
-const pool = new Pool({
-  user: "default",
-  host: "ep-quiet-sun-10474282-pooler.us-east-1.postgres.vercel-storage.com",
-  database: "verceldb",
-  password: "q8hEO7VZtzrl",
-  port: 5432,
-  ssl: { rejectUnauthorized: false },
+// Middleware para manejo de errores centralizado
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
-app.get("/products", (req, res) => {
-  const traerproducts = `SELECT * FROM products `;
-  pool.query(traerproducts).then((data) => {
+// Consulta SQL para insertar un producto
+const insertProductQuery = `INSERT INTO products (NameProduct, Price, Quantity) VALUES($1, $2, $3)`;
+
+// Ruta para obtener todos los productos
+app.get("/products", async (req, res) => {
+  try {
+    const traerproducts = `SELECT * FROM products`;
+    const data = await pool.query(traerproducts);
     console.log("Traemos data", data.rows);
     res.send(data.rows);
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
 });
-app.post("/products", (req, res) => {
-  const Nameproduct = req.body.Nameproduct;
-  const price = req.body.price;
-  const quantity = req.body.quantity;
 
-  const insertProduct = `INSERT INTO products (NameProduct, Price, Quantity) VALUES('${Nameproduct}', ${price}, ${quantity})`;
+// Ruta para agregar un nuevo producto
+app.post("/products", async (req, res, next) => {
+  try {
+    const { Nameproduct, price, quantity } = req.body;
 
-  pool
-    .query(insertProduct)
-    .then(() => {
-      res.status(200).send("Product inserted successfully");
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(err);
-    });
+    // Validación de entrada
+    if (!Nameproduct || typeof price !== 'number' || typeof quantity !== 'number') {
+      return res.status(400).send("Invalid input data");
+    }
+
+    await pool.query(insertProductQuery, [Nameproduct, price, quantity]);
+    res.status(200).send("Product inserted successfully");
+  } catch (error) {
+    next(error);
+  }
 });
-app.put("/products/:id", (req, res) => {
+
+// Ruta para actualizar un producto
+app.put("/products/:id", async (req, res) => {
   const productId = req.params.id;
   const { Nameproduct, price, quantity } = req.body;
 
   const updateProductQuery = `
-          UPDATE products
-          SET nameproduct = '${Nameproduct}', price = ${price}, quantity =${quantity}
-          WHERE id = ${productId}`;
-  pool
-    .query(updateProductQuery)
-    .then((data) => {
-      console.log(`Product with ID ${productId} updated successfully.`);
-      res.send(`Product with ID ${productId} updated successfully.`);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error updating product.");
-    });
+    UPDATE products
+    SET nameproduct = $1, price = $2, quantity = $3
+    WHERE id = $4`;
+
+  try {
+    const data = await pool.query(updateProductQuery, [Nameproduct, price, quantity, productId]);
+    console.log(`Product with ID ${productId} updated successfully.`);
+    res.send(`Product with ID ${productId} updated successfully.`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error updating product.");
+  }
 });
 
-app.delete("/products/:id", (req, res) => {
-  const productsId = req.params.id;
-  const query = `DELETE FROM products WHERE id = $1;`;
+// Ruta para eliminar un producto
+app.delete("/products/:id", async (req, res) => {
+  const productId = req.params.id;
+  const deleteProductQuery = `DELETE FROM products WHERE id = $1`;
 
-  pool
-    .query(query, [productsId])
-    .then((data) => {
-      if (data.rowCount === 0) {
-        res.status(404).send("Producto no encontrado");
-      } else {
-        console.log(`Producto con ID ${productsId} eliminado exitosamente`);
-        res.send(`Producto con ID ${productsId} eliminado exitosamente`);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error en el servidor");
-    });
+  try {
+    const data = await pool.query(deleteProductQuery, [productId]);
+    if (data.rowCount === 0) {
+      res.status(404).send("Product not found");
+    } else {
+      console.log(`Product with ID ${productId} deleted successfully`);
+      res.send(`Product with ID ${productId} deleted successfully`);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting product.");
+  }
 });
-app.listen(PORT, (req, res) => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Manejar eventos de cierre
+process.on('beforeExit', () => pool.end());
+process.on('SIGINT', () => {
+  pool.end();
+  process.exit(0);
 });
